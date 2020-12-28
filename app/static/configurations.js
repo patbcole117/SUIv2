ab3d098114a037d8779abedac7ed9edb76547d8c
Patrick Coleman <pcolemanofficial@gmail.com>
2020-12-27 20:28:53 -0500

    configuration current bout added.

--- app/static/configurations.js
+++ app/static/configurations.js
@@ -1,5 +1,14 @@
 $('#top').load('/e_top');
 $('#clock').load('/e_clock');
+$('#current_bout').load('/e_current_bout_table');
 $('#sbo').load('/e_sbo_config');
 $('#sdc').load('/e_sdc_config');
 $('#sui').load('/e_sui_config');
+
+setInterval(function(){
+    $('#clock').load('/e_clock');
+}, 500);
+
+setInterval(function(){
+    $('#current_bout').load('/e_current_bout_table');
+}, 30000);
