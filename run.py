2183ff326a0e6de153a114f4658795a47df1d5bb
Patrick Coleman <pcolemanofficial@gmail.com>
2021-01-04 06:57:27 -0500

    minor syntax change

--- run.py
+++ run.py
@@ -2,6 +2,5 @@
 from app import app
 from app.utils.conf_parse import get_config
 
-import os
-
-app.run(debug=False, host=os.getenv('SUI_HOST'), port=os.getenv('SUI_PORT'))
+c = get_config()
+app.run(debug=False, host=c['SUI_HOST'], port=c['SUI_PORT'])
